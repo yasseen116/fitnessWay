48c61950608d08647ee22503d2632b0184fee54a
Omar-Yosrii74 <bmr24873@gmail.com>
2025-12-26 02:20:01 +0200

    skill file added

---
+++ backend/models/skill.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from backend.database import Base
+
+class Skill(Base):
+    __tablename__ = "skills"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False) # Tag input
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="skills")
